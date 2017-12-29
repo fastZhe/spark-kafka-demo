f1aac00e7cbcd89ce8aece7a83c98f4f25cc9b1c
huangzhe <526910607@qq.com>
2017-12-29 09:10:13 +0800

    增加注释

--- src/main/java/com/hz/streaming/utils/Constance.java
+++ src/main/java/com/hz/streaming/utils/Constance.java
@@ -1,12 +1,19 @@
 package com.hz.streaming.utils;
 
 public class Constance {
+    //应用名称
     public final static String APP_NAME="kafka-spark";
+    //拉取数据间隔时间
     public final static String APP_DURATION="duration";
+    //spark checkpoint地址
     public final static String APP_CHECKPOINT="/kafka-spark-example";
+    //kafka的server
     public final static String APP_BOOTSTRAP_SERVER="bootstrap.servers";
+    //kafka主题
     public final static String APP_TOPICS="topic";
+    //kafka消费group
     public final static String APP_GROUP_ID="group.id";
+    //saprk处理消息的具体业务类
     public final static String APP_MESSAGE_CLASS="message.class";
 
 
